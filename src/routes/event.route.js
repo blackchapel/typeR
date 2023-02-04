942cb2faeb9fb3c8f4549b3ac08884bfe881b5e1
Vidhita Pai <optimus@vd.local>
2023-02-04 15:18:55 +0530

    added event apis

--- src/routes/event.route.js
+++ src/routes/event.route.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const auth = require('./../middlewares/auth.middleware');
-const { createEvent } = require('../controllers/event.controller');
+const {
+    createEvent,
+    getEventList
+} = require('../controllers/event.controller');
 const upload = require('./../utilities/multer');
 
 // Initializing router
 const router = express.Router();
 
 router.post('/', [auth.verifyJwt], upload.single('thumbnail'), createEvent);
+
+router.get('/', [auth.verifyJwt], getEventList);
 
 module.exports = router;
